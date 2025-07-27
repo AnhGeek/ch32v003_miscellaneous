7e642ce9ac246af2e740c50f48908db8f2a952f1
AnhGeek <lyhoanganh.hcm05@gmail.com>
2025-07-27 13:36:18 +0700

    ch32v003 with SG90 example

---
+++ ch32v003_sg90/User/main.c
@@ -0,0 +1,143 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : main.c
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2024/01/01
+ * Description        : Main program body.
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for 
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
+/*
+ *@Note
+ *GPIO routine:
+ *PD0 push-pull output.
+ *
+*/
+
+#include "debug.h"
+
+/* Global define */
+/* PWM Output Mode Definition */
+#define PWM_MODE1   0
+#define PWM_MODE2   1
+
+/* PWM Output Mode Selection */
+//#define PWM_MODE PWM_MODE1
+#define PWM_MODE PWM_MODE2
+/* Global Variable */
+
+/*********************************************************************
+ * @fn      GPIO_Toggle_INIT
+ *
+ * @brief   Initializes GPIOD.0
+ *
+ * @return  none
+ */
+void GPIO_Toggle_INIT(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    
+}
+/*********************************************************************
+ * @fn      TIM2_OutCompare_Init
+ *
+ * @brief   Initializes TIM1 output compare.
+ *
+ * @param   arr - the period value.
+ *          psc - the prescaler value.
+ *          ccp - the pulse value.
+ *
+ * @return  none
+ */
+void TIM2_PWMOut_Init(u16 arr, u16 psc, u16 ccp)
+{
+    GPIO_InitTypeDef GPIO_InitStructure={0};
+    TIM_OCInitTypeDef TIM_OCInitStructure={0};
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure={0};
+
+    //RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOD | RCC_APB2Periph_TIM1, ENABLE );
+    RCC_APB1PeriphClockCmd( RCC_APB1Periph_TIM2, ENABLE );
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
+    GPIO_Init( GPIOD, &GPIO_InitStructure );
+
+    TIM_TimeBaseInitStructure.TIM_Period = arr;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = psc;
+    TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInit( TIM2, &TIM_TimeBaseInitStructure);
+
+#if (PWM_MODE == PWM_MODE1)
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+
+#elif (PWM_MODE == PWM_MODE2)
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
+
+#endif
+
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = ccp;
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+    TIM_OC4Init( TIM2, &TIM_OCInitStructure );
+
+    TIM_CtrlPWMOutputs(TIM2, ENABLE );
+    TIM_OC4PreloadConfig( TIM2, TIM_OCPreload_Disable );
+    TIM_ARRPreloadConfig( TIM2, ENABLE );
+    TIM_Cmd( TIM2, ENABLE );
+}
+
+/*********************************************************************
+ * @fn      main
+ *
+ * @brief   Main program.
+ *
+ * @return  none
+ */
+int main(void)
+{
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+    SystemCoreClockUpdate();
+    Delay_Init();
+#if (SDI_PRINT == SDI_PR_OPEN)
+    SDI_Printf_Enable();
+#else
+    USART_Printf_Init(115200);
+#endif
+    printf("SystemClk:%d\r\n", SystemCoreClock);
+    printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+    printf("GPIO Toggle TEST\r\n");
+    FLASH_Unlock();
+    FLASH_EraseOptionBytes();
+    FLASH_UserOptionByteConfig(OB_IWDG_SW, OB_STDBY_NoRST, OB_RST_NoEN, OB_PowerON_Start_Mode_USER);
+    FLASH_Unlock();
+    DBGMCU_Config(DBGMCU_IWDG_STOP, DISABLE);
+    
+    
+    GPIO_Toggle_INIT();
+    TIM2_PWMOut_Init( 1000, 480-1, 948 );
+    //TIM2_PWMOut_Init( 100, 120-1, 50);
+
+    while(1)
+    {
+        
+        //TIM2->CH4CVR = 948; // Push
+        GPIO_WriteBit(GPIOD, GPIO_Pin_2, 1);
+        Delay_Ms(500);
+        TIM2->CH4CVR = 935; // Release
+        GPIO_WriteBit(GPIOD, GPIO_Pin_2, 0);
+        while (1) {
+        // Do nothing
+        }
+    }
+}
